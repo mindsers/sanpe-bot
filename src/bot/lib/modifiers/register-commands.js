d14a181fca457226086db17390bd42917b2381d2
Nathanael Cherrier <dev@nathanaelcherrier.com>
2021-04-16 15:09:35 +0200

    :recycle: Reduce cyclomatic complexity

--- src/bot/lib/modifiers/register-commands.js
+++ src/bot/lib/modifiers/register-commands.js
@@ -2,24 +2,30 @@ const commandMemory = {}
 
 export function registerCommands(...commandsAndAliases) {
   const commands = new Map()
-  for (const command of commandsAndAliases) {
-    if (commands.has(command.name)) {
-      throw new Error(`Bad command declaration. There is already a resolver for !${command.name}`)
+  for (const { name, aliases = [], resolver = () => {} } of commandsAndAliases) {
+    if (commands.has(name)) {
+      throw new Error(`Bad command declaration. There is already a resolver for !${name}`)
     }
-    commands.set(command.name, command.resolver)
-    if (command.aliases && Array.isArray(command.aliases)) {
-      for (const alias of command.aliases) {
-        if (alias === command.name) {
-          console.log('Alias refer to the same command')
-          continue
-        }
-        if (commands.has(alias)) {
-          throw new Error(`Bad alias declaration. There is already a resolver for !${alias}`)
-        }
-        commands.set(alias, command.resolver)
+
+    commands.set(name, resolver)
+
+    if (aliases == null || !Array.isArray(aliases)) {
+      continue // no aliases
+    }
+
+    for (const alias of aliases) {
+      if (alias === name) {
+        continue // useless
       }
+
+      if (commands.has(alias)) {
+        throw new Error(`Bad alias declaration. There is already a resolver for !${alias}`)
+      }
+
+      commands.set(alias, resolver)
     }
   }
+
   return (incomingMessage, messageContext) => {
     if (!incomingMessage.text.startsWith('!')) {
       return { ...messageContext } // Ignore if not a command
