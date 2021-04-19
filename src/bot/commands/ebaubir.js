b0131b37a6116b45b4204717a8d334e25eccb821
Nathanael Cherrier <dev@nathanaelcherrier.com>
2021-04-19 20:17:29 +0200

    Fix command imports

--- src/bot/commands/ebaubir.js
+++ src/bot/commands/ebaubir.js
@@ -1,3 +1,5 @@
+import { command } from '../lib/utils/command.js'
+
 function resolver() {
   return `verbe pronominal. Être stupéfait, interdit, marquer une grande surprise. mindse4Shook mindse4Shook mindse4Shook`
 }
