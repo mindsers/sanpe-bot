1feecef9b96af6bfdc00a98575794f9fe73236fd
Nathanael Cherrier <dev@nathanaelcherrier.com>
2021-04-22 21:08:56 +0200

    :recycle: Reduce complexity

--- src/bot/say-hello.js
+++ src/bot/say-hello.js
@@ -1,31 +1,31 @@
 export function sayHello() {
   const helloUserMemory = new Set()
   const hellos = ['hey', 'hi', 'hello', 'cc', 'yo', 'salut', 'bonjour', 'bijour', 'coucou', 'saloute']
-  const botUsername = `@${process.env.BOT_USERNAME}`
 
-  return ({ text }, { username }) => {
+  return ({ text }, messageContext) => {
+    const { username } = messageContext
     const messageWords = text.toLowerCase().split(' ')
-    const containsHello = messageWords.some(v => hellos.indexOf(v) !== -1)
-    let mustAnswer = false
 
-    if (containsHello) {
-      if (messageWords.contains(botUsername)) {
-        //Message is for the bot so it must respond
-        mustAnswer = true
-      } else if (messageWords.filter(w => w.startsWith('@')).length > 0) {
-        // Message for someone else ?
-        mustAnswer = false
-      }
-
-      if (mustAnswer && !helloUserMemory.has(username)) {
-        helloUserMemory.add(username)
-        return {
-          ...messageContext,
-          message: `Hello ${messageContext.displayName}!! Thank you for watching and welcome!`,
-        }
-      }
+    if (!messageWords.some(v => hellos.includes(v))) {
+      return { ...messageContext }
     }
 
-    return { ...messageContext }
+    const mentionSomeone = messageWords.filter(w => w.startsWith('@')).length > 0
+    const mentionSanpe = mentionSomeone && messageWords.includes(`@${process.env.BOT_USERNAME.toLowerCase()}`)
+
+    if (mentionSomeone && !mentionSanpe) {
+      // mention
+      return { ...messageContext }
+    }
+
+    if (helloUserMemory.has(username) && !mentionSanpe) {
+      return { ...messageContext }
+    }
+
+    helloUserMemory.add(username)
+    return {
+      ...messageContext,
+      message: `Hello @${messageContext.displayName}!! Thank you for watching and welcome!`,
+    }
   }
 }
