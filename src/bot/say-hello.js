6cd77cb14cfe0e5a56ebd0522cf195378a24851a
vmathieu <virgile.mathieu@vitibot.fr>
2021-04-22 11:39:10 +0200

    :sparkles: Add intelligence to sayHello handler

--- src/bot/say-hello.js
+++ src/bot/say-hello.js
@@ -1,9 +1,24 @@
 export function sayHello() {
-  return (incomingMessage, messageContext) => {
-    const hellos = ['hey', 'hi', 'hello', 'cc', 'yo', 'salut', 'bonjour', 'bijour', 'coucou', 'saloute']
+  const helloUserMemory = new Set()
+  const hellos = ['hey', 'hi', 'hello', 'cc', 'yo', 'salut', 'bonjour', 'bijour', 'coucou', 'saloute']
+  const botUsername = `@${process.env.BOT_USERNAME}`
 
-    for (const hello of hellos) {
-      if (incomingMessage.text.toLowerCase().split(' ').includes(hello)) {
+  return ({ text }, { username }) => {
+    const messageWords = text.toLowerCase().split(' ')
+    const containsHello = messageWords.some(v => hellos.indexOf(v) !== -1)
+    let mustAnswer = false
+
+    if (containsHello) {
+      if (messageWords.contains(botUsername)) {
+        //Message is for the bot so it must respond
+        mustAnswer = true
+      } else if (messageWords.filter(w => w.startsWith('@')).length > 0) {
+        // Message for someone else ?
+        mustAnswer = false
+      }
+
+      if (mustAnswer && !helloUserMemory.has(username)) {
+        helloUserMemory.add(username)
         return {
           ...messageContext,
           message: `Hello ${messageContext.displayName}!! Thank you for watching and welcome!`,
