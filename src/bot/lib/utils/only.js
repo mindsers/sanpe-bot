2c5a18b3c9cb27181210454ec8df20c48d534c8d
Nathanael Cherrier <dev@nathanaelcherrier.com>
2021-04-24 15:05:43 +0200

    Limit the scope of memory when using only()

--- src/bot/lib/utils/only.js
+++ src/bot/lib/utils/only.js
@@ -1,9 +1,29 @@
 export function only(channel, modifier) {
-  return (incommingMessage, messageContext) => {
-    if (messageContext.channel === channel) {
-      return messageContext
+  return (incommingMessage, previousCtx) => {
+    if (previousCtx.channel === channel) {
+      return {}
     }
 
-    return modifier(incommingMessage, messageContext)
+    const previousMemory = previousCtx.memory || {}
+    const previousChannelMemory = previousMemory[channel] || {}
+
+    const currentCtx = modifier(incommingMessage, {
+      ...previousCtx,
+      memory: previousChannelMemory,
+    })
+
+    const currentChannelMemory = currentCtx.memory || {}
+
+    return {
+      ...previousCtx,
+      ...currentCtx,
+      memory: {
+        ...previousMemory,
+        [channel]: {
+          ...previousChannelMemory,
+          ...currentChannelMemory,
+        },
+      },
+    }
   }
 }
