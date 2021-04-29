56ae1b524d2fecd22c7ea14036f5a0086398a601
Nathanael Cherrier <dev@nathanaelcherrier.com>
2021-04-29 20:38:30 +0200

    :bug: Support async modifiers

--- src/bot/lib/utils/only.js
+++ src/bot/lib/utils/only.js
@@ -1,19 +1,18 @@
 export function onlyFor(channel, modifier) {
   const memoryKey = `${Math.random().toString(36).substring(2)}-${channel}-scoped-memory`
 
-  return (incomingMessage, previousCtx) => {
-    if (previousCtx.channel === channel) {
-      return {}
+  return async (incomingMessage, previousCtx) => {
+    if (previousCtx.broadcaster !== channel) {
+      return previousCtx
     }
 
     const previousMemory = previousCtx.memory || {}
     const previousChannelMemory = previousMemory[memoryKey] || {}
 
-    const currentCtx = modifier(incomingMessage, {
+    const currentCtx = await modifier(incomingMessage, {
       ...previousCtx,
       memory: previousChannelMemory,
     })
-
     const currentChannelMemory = currentCtx.memory || {}
 
     return {
@@ -33,15 +32,15 @@ export function onlyFor(channel, modifier) {
 export async function composeOnlyFor(channel, modifierFactory) {
   const memoryKey = `${Math.random().toString(36).substring(2)}-${channel}-scoped-memory`
 
-  return (...args) => (incomingMessage, previousCtx) => {
-    if (previousCtx.channel === channel) {
-      return {}
+  return (...args) => async (incomingMessage, previousCtx) => {
+    if (previousCtx.broadcaster !== channel) {
+      return previousCtx
     }
 
     const previousMemory = previousCtx.memory || {}
     const previousChannelMemory = previousMemory[memoryKey] || {}
 
-    const currentCtx = modifierFactory(...args)(incomingMessage, {
+    const currentCtx = await modifierFactory(...args)(incomingMessage, {
       ...previousCtx,
       memory: previousChannelMemory,
     })
