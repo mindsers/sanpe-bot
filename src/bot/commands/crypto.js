a1b5b7171529b058771d404c0665bc062fd6188a
Nathanael CHERRIER <nathanael@cherrier.co>
2021-08-23 20:43:08 +0200

    Fix defaults

--- src/bot/commands/crypto.js
+++ src/bot/commands/crypto.js
@@ -2,20 +2,15 @@ import { command } from '../lib/utils/command.js'
 import price from 'crypto-price'
 
 async function resolver({ command: { args } }) {
-  let [cryptoCurrency, currency] = args
-
-  if (currency === null) {
-    currency = 'USD'
-  }
-  if (cryptoCurrency === null) {
-    cryptoCurrency = 'BTC'
-  }
+  const [cryptoCurrency = 'USD', currency = 'BTC'] = args
 
   try {
-    const value = await price.getCryptoPrice(currency, cryptoCurrency)
-    return `1 ${value.base} is worth ${value.price} ${value.target}`
+    const { base, price, target } = await price.getCryptoPrice(currency, cryptoCurrency)
+    
+    return `1 ${base} is worth ${price} ${target}`
   } catch (e) {
     console.error(e)
+    
     return `Unable to retrieve value in ${currency} for crypto ${cryptoCurrency}`
   }
 }
